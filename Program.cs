66b6262365e29ef54bcf62d0f52972b78920c878
Pio <pio.cho.bia@gmail.com>
2023-12-30 13:34:20 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp231229_wlasneWyjatki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
