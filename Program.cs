b5a93687d1cd945dc87948222a8635b9a8b037da
Pio <pio.cho.bia@gmail.com>
2023-12-30 15:23:56 +0100

    wersja finalna 2.0

--- Program.cs
+++ Program.cs
@@ -5,30 +5,38 @@
         static void Main(string[] args)
         {
             string s = "";
-            double r = 0;
+            double a = 0;
             Console.WriteLine("własna obsługa wyjątków");
             Console.WriteLine("\nobliczam pole kwadratu");
-            Console.WriteLine("\npodaj promień");
+            Console.WriteLine("\npodaj bok, nieujemny, mniejszy od 100");
             try
             {
                 s= Console.ReadLine();
-                r=Double.Parse(s);
+                a=Double.Parse(s);
                                
                 //promień r<=0
-                if (r<=0)
+                if (a<=0)
                 {
                     throw new ArgumentOutOfRangeException("","promień nie może być ujemny");
                 }
 
+                if (a>=100) {
+                    throw new ArithmeticException("liczba ponad 100");
+                }
 
+                Console.WriteLine("kwadrat o boku: " + a + " ma pole= " + (a * a));
             }
             catch(ArgumentOutOfRangeException ex) { 
-                Console.WriteLine(ex.Message);
-            
+                Console.WriteLine(ex.Message);            
             }
-
-            
-
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("bład przy parsowaniu stringa do double");
+            }
 
         }
     }
