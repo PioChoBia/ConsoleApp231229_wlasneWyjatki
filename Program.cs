d93f94ee4e4fa6eab5d719e2dce8b0dabc64567b
Pio <pio.cho.bia@gmail.com>
2023-12-30 14:46:33 +0100

    wersja 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = "";
+            double r = 0;
+            Console.WriteLine("własna obsługa wyjątków");
+            Console.WriteLine("\nobliczam pole kwadratu");
+            Console.WriteLine("\npodaj promień");
+            try
+            {
+                s= Console.ReadLine();
+                r=Double.Parse(s);
+                               
+                //promień r<=0
+                if (r<=0)
+                {
+                    throw new ArgumentOutOfRangeException("","promień nie może być ujemny");
+                }
+
+
+            }
+            catch(ArgumentOutOfRangeException ex) { 
+                Console.WriteLine(ex.Message);
+            
+            }
+
+            
+
+
         }
     }
 }
